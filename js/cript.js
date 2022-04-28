77173c6ea0cc9994ed7e363d7cb15b4c2e2096b5
Rafael <rcarneiro782@gmail.com>
2022-04-28 19:53:17 -0300

    part-3 and 4

---
+++ js/cript.js
@@ -0,0 +1,53 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+const score = document.querySelector('.score');
+let isJumping = false;
+let position = 0;
+let playerScore = 0;
+
+const keydownHandler = (e)=>{
+	if(e.keyCode === 32){
+		if(!isJumping){
+			jump();
+		}
+	}
+
+}
+
+const updateScore = ()=>{
+	if(playerScore < 10){
+		score.innerHTML =  'SCORE: 00' + playerScore;
+		return;
+	}
+
+	if(playerScore < 100){
+		score.innerHTML =  'SCORE: 0' + playerScore;
+		return;
+	}
+
+	score.innerHTML =  'SCORE: ' +playerScore;
+	return;
+}
+
+const jump = ()=>{
+	isJumping = true;
+	let speed = 20;
+
+	let upInterval = setInterval(()=>{
+		if(position >= 150){
+			speed *= -1;
+		}
+
+		position += speed;
+		dino.style.bottom = position + 'px';
+
+		if(speed < 0 && position <= 0){
+			position = 0;
+			speed *= -1;
+			clearInterval(upInterval);
+			isJumping = false;
+		}
+	}, 20);
+}
+
+document.addEventListener('keydown', keydownHandler);
